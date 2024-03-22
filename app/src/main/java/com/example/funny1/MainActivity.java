d95053fee153d30eeea5862b6f0c6c5c3900dd47
EasoncoderTW <easonyehmobile1@gmail.com>
2024-03-22 22:08:45 +0800

    fix error

--- app/src/main/java/com/example/funny1/MainActivity.java
+++ app/src/main/java/com/example/funny1/MainActivity.java
@@ -9,24 +9,33 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button1 = findViewById(R.id.button1);
-    Button button2 = findViewById(R.id.button2);
-    TextView textView1 = findViewById(R.id.textview1);
+    Button button1;
+    Button button2;
+    TextView textView1;
     int a;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        textView1 = findViewById(R.id.textview1);
         a = 0;
-        button1.setOnClickListener(v -> {
-                    a = a + 1;
-                    textView1.setText(String.valueOf(a));
-                });
-        button2.setOnClickListener(v -> {
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                a = a + 1;
+                textView1.setText(""+a);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 a = a - 1;
-                textView1.setText(String.valueOf(a));
-            });
+                textView1.setText(""+a);
+            }
+        });
     }
 
 
